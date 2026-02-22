1876c5e565594efe9162ddd8de435f7896816645
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-22 22:38:41 +0530

    Update 01_series_basics.py

--- 01_beginner/01_series_basics.py
+++ 01_beginner/01_series_basics.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+data = [10, 20, 30, 40, 50]
+series = pd.Series(data, index=["a", "b", "c", "d", "e"])
+
+print("Series:\n", series)
+print("Values:", series.values)
+print("Index:", series.index)
+print("Access by label:", series["c"])
+print("Access by position:", series.iloc[2])
+series["c"] = 100
+print("Updated Series:\n", series)
