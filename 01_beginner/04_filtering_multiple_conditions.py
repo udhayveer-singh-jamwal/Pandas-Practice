56ce28114e3e8e922dc0592d21184c0734727d17
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-23 22:17:03 +0530

    Update 04_filtering_multiple_conditions.py

--- 01_beginner/04_filtering_multiple_conditions.py
+++ 01_beginner/04_filtering_multiple_conditions.py
@@ -5,6 +5,7 @@ data = {
     "Age": [23, 30, 22, 28, 35],
     "Salary": [40000, 70000, 35000, 60000, 80000]
 }
-
 df = pd.DataFrame(data)
+filtered = df[(df["Age"] > 25) & (df["Salary"] > 50000)]
 print("Original Data:\n", df)
+print("\nFiltered Data:\n", filtered)
