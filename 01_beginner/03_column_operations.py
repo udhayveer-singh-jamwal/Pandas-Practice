7f888378c623c8a7f4633f2b6c42cc715356a1b7
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-23 22:14:48 +0530

    Update 03_column_operations.py

--- 01_beginner/03_column_operations.py
+++ 01_beginner/03_column_operations.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+data = {
+    "Name": ["A", "B", "C", "D"],
+    "Marks": [85, 90, 78, 88]
+}
+
+df = pd.DataFrame(data)
+df["Bonus"] = df["Marks"] * 0.05
+df["Total"] = df["Marks"] + df["Bonus"]
+df["Grade"] = df["Marks"].apply(lambda x: "A" if x >= 85 else "B")
+print(df)
