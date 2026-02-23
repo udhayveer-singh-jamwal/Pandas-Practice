4d74f5bc7a0421a4de724190216b57eb6ad9223a
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-23 22:16:13 +0530

    Update 04_filtering_multiple_conditions.py

--- 01_beginner/04_filtering_multiple_conditions.py
+++ 01_beginner/04_filtering_multiple_conditions.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+data = {
+    "Name": ["A", "B", "C", "D", "E"],
+    "Age": [23, 30, 22, 28, 35],
+    "Salary": [40000, 70000, 35000, 60000, 80000]
+}
+
+df = pd.DataFrame(data)
+print("Original Data:\n", df)
