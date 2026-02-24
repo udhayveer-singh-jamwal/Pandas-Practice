274d72099d390491f871a6df49d486db71acc1a1
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-24 22:13:13 +0530

    Update 05_sorting_and_ranking.py

--- 01_beginner/05_sorting_and_ranking.py
+++ 01_beginner/05_sorting_and_ranking.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+data = {
+    "Employee": ["A", "B", "C", "D"],
+    "Performance": [75, 90, 85, 80]
+}
+
+df = pd.DataFrame(data)
+df_sorted = df.sort_values(by="Performance", ascending=False)
+print("Sorted Data:\n", df_sorted)
