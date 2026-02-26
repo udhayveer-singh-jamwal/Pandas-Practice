497afb3ea9abff7b66e44206550a11f503933ed5
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-26 22:52:12 +0530

    Update 07_apply_and_lambda.py

--- 01_beginner/07_apply_and_lambda.py
+++ 01_beginner/07_apply_and_lambda.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+data = {
+    "Name": ["A", "B", "C", "D"],
+    "Marks": [45, 78, 88, 32]
+}
+
+df = pd.DataFrame(data)
+def result(marks):
+    if marks >= 50:
+        return "Pass"
+    else:
+        return "Fail"
+
+df["Result"] = df["Marks"].apply(result)
+print(df)
