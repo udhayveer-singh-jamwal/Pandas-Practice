d728a7337d1c76ee9b73e3a7a02e47870ea97b34
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-26 22:48:37 +0530

    Add basic statistics calculations for sales data

--- 01_beginner/06_basic_statistics.py
+++ 01_beginner/06_basic_statistics.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+data = {
+    "Sales": [1200, 1500, 1800, 2000, 1700, 1600]
+}
+df = pd.DataFrame(data)
+print("Total Sales:", df["Sales"].sum())
+print("Average Sales:", df["Sales"].mean())
+print("Median:", df["Sales"].median())
+print("Standard Deviation:", df["Sales"].std())
+print("Variance:", df["Sales"].var())
