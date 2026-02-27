7d141907a2d35b4afe8a2345b8569682f6ec328c
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-27 22:20:41 +0530

    Update 08_drop_rename_columns.py

--- 01_beginner/08_drop_rename_columns.py
+++ 01_beginner/08_drop_rename_columns.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+data = {
+    "Name": ["Aman", "Riya", "Karan"],
+    "Age": [23, 25, 22],
+    "Salary": [40000, 50000, 35000]
+}
+
+df = pd.DataFrame(data)
+df.rename(columns={"Salary": "Monthly_Salary"}, inplace=True)
+print(df)
