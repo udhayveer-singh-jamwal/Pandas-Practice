1f59150eaa3d8e642368c0c16b77f6ec4d4297f7
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-27 22:17:02 +0530

    Add value counts analysis for department data

--- 01_beginner/07_value_counts_analysis.py
+++ 01_beginner/07_value_counts_analysis.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+data = {
+    "Department": ["IT", "HR", "IT", "Finance", "HR", "IT"]
+}
+df = pd.DataFrame(data)
+counts = df["Department"].value_counts()
+print("Department Distribution:\n", counts)
