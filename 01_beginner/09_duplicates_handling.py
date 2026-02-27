1353ebabbbd0a81204b4a35af64e8a670cb8c115
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-27 22:23:09 +0530

    Add duplicate handling with pandas in Python

--- 01_beginner/09_duplicates_handling.py
+++ 01_beginner/09_duplicates_handling.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+data = {
+    "Name": ["A", "B", "A", "C", "B"],
+    "Marks": [85, 90, 85, 78, 90]
+}
+
+df = pd.DataFrame(data)
+duplicates = df.duplicated()
+print("Duplicate Rows:\n", duplicates)
+df_cleaned = df.drop_duplicates()
+print("After Removing Duplicates:\n", df_cleaned)
