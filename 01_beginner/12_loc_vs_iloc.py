d6df295782801145f67687609e7e7631b48485d8
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-01 23:03:30 +0530

    Update 12_loc_vs_iloc.py

--- 01_beginner/12_loc_vs_iloc.py
+++ 01_beginner/12_loc_vs_iloc.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+data = {
+    "Name": ["A", "B", "C", "D"],
+    "Marks": [45, 78, 88, 32]
+}
+df = pd.DataFrame(data)
+print("Using loc:\n", df.loc[1:2])
+print("Using iloc:\n",df.iloc[1:3])
+print("Single value using loc:", df.loc[0, "Marks"])
+print("Single value using iloc:", df.iloc[0, 1])
