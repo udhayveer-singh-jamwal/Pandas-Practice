74ae47ceee1370252200acae006dbc726f7db003
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-02 23:12:55 +0530

    Update 13_boolean_masking_update.py

--- 01_beginner/13_boolean_masking_update.py
+++ 01_beginner/13_boolean_masking_update.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+data = {
+    "Student": ["A", "B", "C", "D"],
+    "Marks": [35, 78, 48, 90]
+}
+df = pd.DataFrame(data)
+df.loc[df["Marks"] < 50, "Marks"] = 50
+df["Status"] = df["Marks"].apply(lambda x: "Pass" if x >= 50 else "Fail")
+print(df)
