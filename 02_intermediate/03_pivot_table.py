45608ca92531e87f49883d8c19051b870a2c903b
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-04 22:24:55 +0530

    Add pivot table creation using pandas

--- 02_intermediate/03_pivot_table.py
+++ 02_intermediate/03_pivot_table.py
@@ -1,1 +1,14 @@
+import pandas as pd
+data = {
+    "Region": ["North", "South", "North", "South", "North"],
+    "Product": ["A", "A", "B", "B", "A"],
+    "Sales": [100, 150, 200, 250, 120]
+}
 
+df = pd.DataFrame(data)
+pivot = pd.pivot_table(
+    df,
+    values="Sales",
+    aggfunc="sum"
+)
+print("Pivot Table:\n", pivot)
