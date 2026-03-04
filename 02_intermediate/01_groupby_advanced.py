d140e38a85f77996d0026faae3cb4b3fe63a859d
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-04 22:22:09 +0530

    Update 01_groupby_advanced.py

--- 02_intermediate/01_groupby_advanced.py
+++ 02_intermediate/01_groupby_advanced.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+data = {
+    "Department": ["IT", "HR", "IT", "Finance", "HR", "IT"],
+    "Salary": [60000, 45000, 70000, 50000, 55000, 80000],
+    "Experience": [3, 5, 4, 6, 2, 7]
+}
+df = pd.DataFrame(data)
+grouped = df.groupby("Department").agg({
+    "Salary": ["mean", "max", "min"],
+    "Experience": "mean"
+})
+print("Grouped Analysis:\n", grouped)
