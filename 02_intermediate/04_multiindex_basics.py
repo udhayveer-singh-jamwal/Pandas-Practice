79098b0dc866d79dc4db984d787462c100af45e9
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-05 23:12:44 +0530

    Update 04_multiindex_basics.py

--- 02_intermediate/04_multiindex_basics.py
+++ 02_intermediate/04_multiindex_basics.py
@@ -1,1 +1,11 @@
+import pandas as pd
+data = {
+    "Region": ["North", "North", "South", "South"],
+    "Year": [2024, 2025, 2024, 2025],
+    "Sales": [1000, 1500, 1200, 1700]
+}
 
+df = pd.DataFrame(data)
+multi = df.set_index(["Region", "Year"])
+print("MultiIndex Data:\n", multi)
+print("\nAccess North 2025:\n", multi.loc[("North", 2025)])
