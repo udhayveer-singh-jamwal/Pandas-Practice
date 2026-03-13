2cf7b2a14d67d36fd2ff5e226a924c6f2e9c0ccd
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-13 22:33:14 +0530

    Update 01_advanced_groupby_transform.py

--- 03_advanced/01_advanced_groupby_transform.py
+++ 03_advanced/01_advanced_groupby_transform.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+data = {
+    "Department": ["IT", "IT", "HR", "HR", "Finance"],
+    "Employee": ["A", "B", "C", "D", "E"],
+    "Salary": [60000, 80000, 50000, 55000, 70000]
+}
+
+df = pd.DataFrame(data)
+df["Dept_Avg"] = df.groupby("Department")["Salary"].transform("mean")
+df["Above_Avg"] = df["Salary"] > df["Dept_Avg"]
+print(df)
