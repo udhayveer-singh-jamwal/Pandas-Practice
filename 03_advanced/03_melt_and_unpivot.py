b8ad517f55629ac7eed19a8ffe6a79bafde771b4
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-16 22:27:18 +0530

    Update 03_melt_and_unpivot.py

--- 03_advanced/03_melt_and_unpivot.py
+++ 03_advanced/03_melt_and_unpivot.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+data = {
+    "Student": ["A", "B", "C"],
+    "Math": [85, 90, 78],
+    "Science": [80, 88, 75]
+}
+
+df = pd.DataFrame(data)
+melted = pd.melt(
+    df,
+    id_vars="Student",
+    var_name="Subject",
+    value_name="Marks"
+)
+print(melted)
