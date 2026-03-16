2fcc9297ef25be1dada0b6e1cb6217b51a7b5679
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-16 22:29:32 +0530

    Update 04_complex_pivot_analysis.py

--- 03_advanced/04_complex_pivot_analysis.py
+++ 03_advanced/04_complex_pivot_analysis.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+data = {
+    "Region": ["North", "North", "South", "South"],
+    "Product": ["A", "B", "A", "B"],
+    "Sales": [1000, 1500, 2000, 1800]
+}
+
+df = pd.DataFrame(data)
+pivot = pd.pivot_table(
+    df,
+    values="Sales",
+    index="Region",
+    columns="Product",
+    aggfunc=["sum", "mean"]
+)
+print(pivot))
