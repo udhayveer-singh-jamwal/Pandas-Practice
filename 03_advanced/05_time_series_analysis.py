ebf199b108120c4dae0d18ea25111f8ab4396284
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-03-17 22:21:40 +0530

    Update 05_time_series_analysis.py

--- 03_advanced/05_time_series_analysis.py
+++ 03_advanced/05_time_series_analysis.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+dates = pd.date_range("2025-01-01", periods=10)
+data = {
+    "Date": dates,
+    "Sales": [1000, 1200, 1500, 1700, 1600, 1800, 2000, 2100, 2200, 2300]
+}
+df = pd.DataFrame(data)
+df.set_index("Date", inplace=True)
+monthly = df.resample("M").sum()
+print("Daily Data:\n", df)
